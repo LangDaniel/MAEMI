bc144e38c45cd5c77a9e8822bebb4c3c101ee9eb
Daniel Lang <lang@helmholtz-munich.de>
2023-05-09 11:21:43 +0200

    training params

--- training/train.sh
+++ training/train.sh
@@ -33,11 +33,11 @@ done
 
 set -- "${POSITIONAL_ARGS[@]}" # restore positional parameters
 
-conda activate mae 
+#conda activate mae 
 if [ ${FOREGROUND} -eq 1 ]; then
         taskset -c ${CPU} python train.py $1
 else
         nohup taskset -c ${CPU} python train.py $1 &
 fi
 printf "$1: $!\n" >> log 
-conda deactivate
+#conda deactivate
